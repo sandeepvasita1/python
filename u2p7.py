dbbb70e93f98d4e15be3890f2f35848647d84162
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-06-20 19:56:51 +0530

    Add files via upload

---
+++ u2p7.py
@@ -0,0 +1,7 @@
+def display(lst):
+    print("disply fruits list:")
+    for i in lst:
+        print(i)
+
+fruits=["apple","mango","banana","cevi"]
+display(fruits)
---
+++ u2p8.py
@@ -0,0 +1,33 @@
+def pos(x,y):
+    print(x+y)
+
+a=2
+b=3
+pos(a,b)
+
+def keyword(i,p):
+    print("item",i)
+    print("price",p)
+
+keyword(i="sugar", p=35.4)
+keyword(i="jaggery",p=78)
+
+
+def default(i,j,p=70.5):
+    print("item",i)
+    print("bath item",j)
+    print("price",p)
+
+default(i="tea", j="soap")
+default(i="grain",j="sampoo")
+
+def abc(f,*f1):
+    sum=0
+    for i in f1:
+        sum+=i
+    print("sum of variable length:",sum)
+    print("sum of formal+variable length:",f+sum)
+
+abc(10)
+abc(10,20)
+abc(10,20,30)
