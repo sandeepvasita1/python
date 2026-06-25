e14e5606580a10a7c86cc15d0fc3707269393221
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-06-25 19:23:46 +0530

    Add files via upload

---
+++ u2p10.py
@@ -0,0 +1,21 @@
+import employee
+
+bsal=int(input("enter the basic salary"))
+
+hra=employee.hra(bsal)
+da=employee.da(bsal)
+
+g_sal=bsal+hra+da
+
+print(f"hra:{hra:.2f}")
+print(f"da:{da:.2f}")
+print(f"gross salary:{g_sal:.2f}")
+
+pf=employee.pf(g_sal)
+itax=employee.itax(g_sal)
+
+n_sal=g_sal-(pf+itax)
+
+print(f"pf:{pf:.2f}")
+print(f"itax:{itax:.2f}")
+print(f"n_sal:{n_sal:.2f}")
---
+++ u2p11.py
@@ -0,0 +1,14 @@
+lst=list(range(1,15))
+print("print element",lst)
+
+lst.append(25)
+print("add new elements",lst)
+
+lst[5]=45
+print("add new element in",lst)
+
+del lst[7]
+print("delect element",lst)
+
+lst.remove(10)
+print("remove element",lst)
