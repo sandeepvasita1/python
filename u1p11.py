cd0252d4d5fd959d19f0fa65ba2e93459ac881ee
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-06-27 17:24:59 +0530

    Update u1p11.py

--- u1p11.py
+++ u1p11.py
@@ -1,3 +1,9 @@
+Write a python program that asks the user to enter a length in 
+centimeters. If the user enters a negative length, the program 
+should tell the user that the entry is invalid. Otherwise, the 
+program should convert the length to inches and print out the 
+result. (2.54 = 1 inch) 
+
 cm=float(input("enter the length in cm:"))
 if cm<0:
     print("invalid entry")
