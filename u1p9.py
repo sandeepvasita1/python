c35ba473563b48e2e1647cd1a463f2e758d91a23
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-06-27 17:23:36 +0530

    Update u1p9.py

--- u1p9.py
+++ u1p9.py
@@ -1,3 +1,6 @@
+Write a program to assert the user enters a number greater 
+than zero
+
 a=int(input("enter the number"))
 assert a>0,"number should be greate tha zero"
 print("number print out of assert")
