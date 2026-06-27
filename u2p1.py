41dad55627fc01bce34fcebb1f3676d645159e4c
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-06-27 17:25:38 +0530

    Update u2p1.py

--- u2p1.py
+++ u2p1.py
@@ -1,3 +1,6 @@
+Create a program to retrieve, display and update only a range 
+of elements from an array using indexing and slicing in
+
 import array as ar
 arr=ar.array('i',[10,20,30,40,50,60,70,80])
 
