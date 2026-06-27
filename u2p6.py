784984f29003c83b84232a9cfa858ced2c974c8a
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-06-27 17:27:53 +0530

    Update u2p6.py

--- u2p6.py
+++ u2p6.py
@@ -1,3 +1,7 @@
+Write a python program that removes any repeated items 
+from a list so that each item appears at most once. For 
+instance,the list [1,1,2,3,4,3,0,0] would become [1,2,3,4,0]. 
+
 def unique_val(lst):
     r=[]
     for i in lst:
