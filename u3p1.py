72b5e1af18f5993e92c95813b32a63d664e0bb6d
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-07-07 16:37:31 +0530

    Remove college name from student display method
    Removed printing of college name in display method.

---
+++ u3p1.py
@@ -0,0 +1,18 @@
+class student:
+   def __init__ (self,rno,n,m):
+        self.rollno=rno
+        self.name=n
+        self.mark=m
+
+    def display(self):
+        print(f"student rollno is:{self.rollno}")
+        print(f"student name is:{self.name}")
+        print(f"student mark is:{self.mark}")
+        print("")
+
+
+s1=student(101,"sandeep",85)
+s1.display()
+
+s2=student(102,"suhani",78)
+s2.display()
