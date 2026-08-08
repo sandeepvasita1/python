530d5092ad9e845c29affc443f43d868bfabc626
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-08-08 08:20:46 +0530

    Add second sorting method for tuples in u2p15.py
    Added a second method to sort the tuple and demonstrated its usage.

--- u2p15.py
+++ u2p15.py
@@ -10,4 +10,14 @@ for i in tpl:
 print("sorted tuple:",sorted(tpl,reverse=True))
 print("sorted tuple by name:",sorted(tpl,key=lambda x:x[1]))
 print("sorted tuple by salary:",sorted(tpl,key=lambda x:x[2])) 
+
+---->second method to short the tuple
+tpl=((205,'sandeep',25000),
+     (102,'suhani',20000),
+     (401,'priya',30000),
+     (304,'khushi',15000))
+print(tpl)
+s = tuple(sorted(tpl))
+
+print("Sorted tuple:", s)
      
