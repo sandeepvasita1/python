5eac46a3c6ba1605074586bda6da15b104c73c92
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-08-21 19:14:06 +0530

    Add files via upload

---
+++ u4p4.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+numbers = [10, 20, 30, 40, 50]
+
+
+series = pd.Series(numbers)
+
+print(series)
---
+++ u4p6.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+data = {
+    "Name": ["Sandeep", "suhani", "krima"],
+    "Age": [20, 21, 20],
+    "Marks": [85, 78, 90]
+}
+
+
+df = pd.DataFrame(data)
+
+
+print(df)
