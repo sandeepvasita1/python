368372c91c8d2a5a6f15bd52376d6b0ce6af708c
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-08-21 19:27:55 +0530

    Update u4p5.py
    Rearranged import statement and added comment about saving the file as CSV.

--- u4p5.py
+++ u4p5.py
@@ -1,3 +1,4 @@
+#create in excel file to insert the data and save File → Save As → CSV (Comma delimited) (*.csv) select karo. in sem folder python file is here
 import pandas as pd
 
 df = pd.read_csv("Book1.csv")
