eb92d8564fc7799ca6741b7ad5202dbed191a5fd
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-09-16 11:00:47 +0530

    Update u4p12.py

--- u4p12.py
+++ u4p12.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 
 eid=[101,102,103,104,105]
-ename=['sandeep','suhani','krima','priya','khushi']
+ename=['sandeep','suhani','gunjan','priya','khushi']
 sal=[20000,50000,90000,70000,60000]
 
 plt.bar(eid,sal,color='pink')
