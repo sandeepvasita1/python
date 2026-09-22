b8f80fd8139168f6dde3e82859f46aa21eb243be
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-09-22 15:38:42 +0530

    Add files via upload

---
+++ u2p18.py
@@ -0,0 +1,13 @@
+def display_dict(d):
+    for key, value in d.items():
+        print(key, ":", value)
+
+
+# Dictionary
+student = {
+    "name": "Sandeep",
+    "age": 21,
+    "city": "Ahmedabad"
+}
+
+display_dict(student)
