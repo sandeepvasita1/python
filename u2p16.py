bae3ef32cb25d99cefc24ce1f8fe3a96d853fd73
sandeepvasita1 <sandeepvasita93@gmail.com>
2026-09-22 15:20:14 +0530

    Update u2p16.py
    Updated the code to include comments explaining the functionality.

--- u2p16.py
+++ u2p16.py
@@ -1,3 +1,6 @@
+Create a dictionary that will accept cricket players name and 
+scores in a match. Also we are retrieving runs by entering the 
+player’s name. 
 p={'kohli':105,'dhoni':150,'rohit':200,'sachin':250}
 print(p)
 
